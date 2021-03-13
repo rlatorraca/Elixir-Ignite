624c0424ec3ea98271cdd8e14036d9a7ab218518
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-03-13 15:24:53 -0400

    Add test to Call method

--- sum_list/test/sum_list_test.exs
+++ sum_list/test/sum_list_test.exs
@@ -1,8 +1,15 @@
 defmodule SumListTest do
-  use ExUnit.Case
-  doctest SumList
+  use ExUnit.Case # Traz todas funcionalidades da suite de testes
+  #doctest SumList => Executa e testa a documentacao
 
-  test "greets the world" do
-    assert SumList.hello() == :world
+  describe "call/1" do
+    test "returns the lsit sum" do
+      list = [ 1 , 2 , 3, 4]
+
+      response = SumList.call(list)
+      expected_response = 10
+
+      assert response == expected_response
+    end
   end
 end
