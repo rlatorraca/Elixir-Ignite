3a76907d5bd31a1c0f68fb0d6e220e1d16b1eaa3
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-03-13 22:30:44 -0400

    elixirls

--- sum_list/lib/sum_list.ex
+++ sum_list/lib/sum_list.ex
@@ -1,6 +1,10 @@
 defmodule SumList do
   def call(list), do: sum(list, 0)
 
+  def call_enum(list), do: Enum.map(list, fn elem -> elem + 1 end)
+
+  def call_enum_greater(list), do: Enum.any?(list, fn elem -> elem > 5 end)
+
   defp sum([], acc), do: acc
 
   defp sum([head | tail] = _list, acc) do
