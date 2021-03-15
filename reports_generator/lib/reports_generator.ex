40a88724f82a68f22f9a6b35f8c94852d4e23286
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-03-15 00:15:08 -0300

    Reading file by File.stream

--- reports_generator/lib/reports_generator.ex
+++ reports_generator/lib/reports_generator.ex
@@ -13,11 +13,26 @@ defmodule ReportsGenerator do
 
     # Mode 2
     "reports/#{filename}"
-      |> File.read()
-      |> handle_file("Second argument")
+      # Mode 2.1
+      # |> File.read()
+      # |> handle_file("Second argument")
+      
+      # Mode 2.2
+      |> File.stream!()
+      |> Enum.map(fn line -> parse_line(line) end) # &parse_line(&1)
+     
 
   end
 
+  # Mode 2.1
   defp handle_file({:ok, file_content}, msg), do: file_content <> " => " <> msg
   defp handle_file({:error, _reason}, msg), do: "Error while opening file !!! " <> msg
+
+  # Mode 2.2
+  defp parse_line(line) do
+    line 
+      |> String.trim()
+      |> String.split(",")
+      |> List.update_at(2, &String.to_integer/1) # fn elem -> String.to_integer(elem) end
+  end
 end
