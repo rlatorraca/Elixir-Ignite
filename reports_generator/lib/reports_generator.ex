6b26ce2c543cad7adaefde09fa748a5e768e971d
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-03-17 00:49:28 -0300

    Changed "MODE" to  "VERSION"

--- reports_generator/lib/reports_generator.ex
+++ reports_generator/lib/reports_generator.ex
@@ -3,7 +3,7 @@ defmodule ReportsGenerator do
   def build (filename) do
 
     """
-    Mode 1
+    Version 1
     case File.read("reports/#{filename}") do
       {:ok, result} -> result
       {:error, reason} -> reason
@@ -11,17 +11,17 @@ defmodule ReportsGenerator do
     end
     """
 
-    # Mode 2
+    # Version 2
     "reports/#{filename}"
-      # Mode 2.1
+      # Version 2.1
       # |> File.read()
       # |> handle_file("Second argument")
       
-      # Mode 2.2
+      # Version 2.2
       # |> File.stream!()
       # |> Enum.map(fn line -> parse_line(line) end) # &parse_line(&1)
 
-      # Mode 2.3
+      # Version 2.3
       |> File.stream!()
       |> Enum.reduce(report_accumulator(), fn line, report -> 
             [id, _foodName, price] = parse_line(line) 
@@ -29,11 +29,11 @@ defmodule ReportsGenerator do
           end) 
   end
 
-  # Mode 2.1
+  # Version 2.1
   # defp handle_file({:ok, file_content}, msg), do: file_content <> " => " <> msg
   # defp handle_file({:error, _reason}, msg), do: "Error while opening file !!! " <> msg
 
-  # Mode 2.2
+  # Version 2.2
   defp parse_line(line) do
     line 
       |> String.trim()
@@ -41,6 +41,6 @@ defmodule ReportsGenerator do
       |> List.update_at(2, &String.to_integer/1) # fn elem -> String.to_integer(elem) end
   end
 
-  # Mode 2.3
+  # Version 2.2 and Version 2.3
   defp report_accumulator, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
