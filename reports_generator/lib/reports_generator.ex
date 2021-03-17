8efd12e19af3f7938ec50abd9fac5c60bddf8dc8
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-03-17 00:47:39 -0300

    Version using Enum.reduce, Map.put and Enum.into.

--- reports_generator/lib/reports_generator.ex
+++ reports_generator/lib/reports_generator.ex
@@ -18,15 +18,20 @@ defmodule ReportsGenerator do
       # |> handle_file("Second argument")
       
       # Mode 2.2
-      |> File.stream!()
-      |> Enum.map(fn line -> parse_line(line) end) # &parse_line(&1)
-     
+      # |> File.stream!()
+      # |> Enum.map(fn line -> parse_line(line) end) # &parse_line(&1)
 
+      # Mode 2.3
+      |> File.stream!()
+      |> Enum.reduce(report_accumulator(), fn line, report -> 
+            [id, _foodName, price] = parse_line(line) 
+            Map.put(report, id, report[id] + price)
+          end) 
   end
 
   # Mode 2.1
-  defp handle_file({:ok, file_content}, msg), do: file_content <> " => " <> msg
-  defp handle_file({:error, _reason}, msg), do: "Error while opening file !!! " <> msg
+  # defp handle_file({:ok, file_content}, msg), do: file_content <> " => " <> msg
+  # defp handle_file({:error, _reason}, msg), do: "Error while opening file !!! " <> msg
 
   # Mode 2.2
   defp parse_line(line) do
@@ -35,4 +40,7 @@ defmodule ReportsGenerator do
       |> String.split(",")
       |> List.update_at(2, &String.to_integer/1) # fn elem -> String.to_integer(elem) end
   end
+
+  # Mode 2.3
+  defp report_accumulator, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
