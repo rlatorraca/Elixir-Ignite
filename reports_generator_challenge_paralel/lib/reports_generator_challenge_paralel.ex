239c104582ffcc0e80ddaff3cdf147ecc1b8d658
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-03-22 13:52:02 -0300

    Iniial paralel challenge

---
+++ reports_generator_challenge_paralel/lib/reports_generator_challenge_paralel.ex
@@ -0,0 +1,18 @@
+defmodule ReportsGeneratorChallengeParalel do
+  @moduledoc """
+  Documentation for `ReportsGeneratorChallengeParalel`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ReportsGeneratorChallengeParalel.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ reports_generator_challenge_paralel/test/reports_generator_challenge_paralel_test.exs
@@ -0,0 +1,8 @@
+defmodule ReportsGeneratorChallengeParalelTest do
+  use ExUnit.Case
+  doctest ReportsGeneratorChallengeParalel
+
+  test "greets the world" do
+    assert ReportsGeneratorChallengeParalel.hello() == :world
+  end
+end
