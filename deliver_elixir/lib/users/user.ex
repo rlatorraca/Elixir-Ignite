b12e8d83827e928381f80f9ec8bd6ff2eb25aa12
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-03-25 22:56:28 -0300

    Structs in Elixir examples

---
+++ deliver_elixir/lib/users/user.ex
@@ -0,0 +1,5 @@
+defmodule DeliverElixir.Users.User do
+  #  Structs example => e um MAPA com um NOME
+  ## usamos apra envelopar os dados e transportar de um lado par aoutro (como classe ou model)
+  defstruct [:name, :email, :cpf, :age]
+end
