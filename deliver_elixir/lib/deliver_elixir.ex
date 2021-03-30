df80c76236d884bb37e3d07540214ca78ac2396d
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-03-30 00:05:23 -0300

    Added facade design patterns to create an User by Agent

--- deliver_elixir/lib/deliver_elixir.ex
+++ deliver_elixir/lib/deliver_elixir.ex
@@ -1,2 +1,11 @@
 defmodule DeliverElixir do
+  alias DeliverElixir.Users.Agent, as: UserAgent
+  alias DeliverElixir.Users.CreateOrUpdate
+
+  # Design Patterns => FACADE
+  defdelegate create_or_update_user(params), to: CreateOrUpdate, as: :call
+
+  def starts_agents do
+    UserAgent.start_link(%{})
+  end
 end
